25b9e0eae437d6e5c82c5f0accd559dcd493c596
jsj7878 <61611131+jsj7878@users.noreply.github.com>
2021-01-08 14:15:22 +0900

    add image uploader

--- app/src/main/java/com/example/week2/Page2Fragment.java
+++ app/src/main/java/com/example/week2/Page2Fragment.java
@@ -1,20 +1,27 @@
 package com.example.week2;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+
+import static com.example.week2.MainActivity.testlist;
 
 public class Page2Fragment extends Fragment {
 
@@ -56,68 +63,26 @@ public class Page2Fragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.page2fragment, null);
+        Intent intent = new Intent(getActivity(), AddImageActivity.class);
+        ImageButton imgAddButton =  view.findViewById(R.id.addImageButton);
+        imgAddButton.setOnClickListener(new View.OnClickListener(){
+
+             @Override
+             public void onClick(View v) {
+                 startActivity(intent);
+             }
+         });
+
+        return view;
     }
 
 
 }
 
-/*class GridAdapter extends BaseAdapter{
-    Context context;
-    int layout;
-    ArrayList<GalleryImage> img;
-    LayoutInflater inf;
-
-
-    public GridAdapter(Context context, int layout, ArrayList<GalleryImage> img){
-        this.context = context;
-        this.layout = layout;
-        this.img = img;
-        inf = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
-
-    @Override
-    public int getCount() {
-        return img.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return img.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView = new ImageView(context);
-
-        //Point display = getActivity().getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-       // display.getRealSize(size); // or getSize(size)
-        int width = size.x;
-
-        width /= 3;
-        width -= 10;
-
-        imageView.setLayoutParams(new GridView.LayoutParams(width, width));
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        imageView.setPadding(5, 5, 5, 5);
-
-        imageView.setImageBitmap(img.get(position).getRd());
-        final int pos = position;
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-
-        return imageView;
-    }
-}*/
 
