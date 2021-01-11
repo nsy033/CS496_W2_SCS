9106cc806e44d4720bb59e628bc7f03cb147a400
sy <nsy033@kaist.ac.kr>
2021-01-12 02:07:11 +0900

    playlist

--- app/src/main/java/com/example/week2/Page3Fragment.java
+++ app/src/main/java/com/example/week2/Page3Fragment.java
@@ -1,8 +1,31 @@
 package com.example.week2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Page3Fragment extends Fragment {
 
@@ -14,6 +37,11 @@ public class Page3Fragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    static ArrayList<PlayList> playLists = new ArrayList<>();
+
+    private static final String API_KEY = "AIzaSyANYM5TIbJohkt1_z0P48A4WB8IEr2cVe0";
+    private static String VIDEO_ID = "STwHSJSA86c";
 
     public Page3Fragment() {
         // Required empty public constructor
@@ -45,4 +73,89 @@ public class Page3Fragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        String url = "http://www.youtube.com";
+
+        PlayList song = new PlayList();
+        Date date = new Date();
+        song.setKeys("Ib5ec71QIc8");
+        song.setTime(date);
+        song.setExplain("Ed Sheeran - Afterglow");
+        playLists.add(song);
+
+        PlayList song2 = new PlayList();
+        Date date2 = new Date();
+        song2.setKeys("DvC3MdUzjmM");
+        song2.setTime(date2);
+        song2.setExplain("뉴홉클");
+        playLists.add(song2);
+
+        WebView view = rootView.findViewById(R.id.home_webview);
+        view.setWebViewClient(new WebViewClient());
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                //This is the filter
+                if (event.getAction()!=KeyEvent.ACTION_DOWN)
+                    return true;
+
+                if (keyCode == KeyEvent.KEYCODE_BACK) {
+                    if (view.canGoBack()) {
+                        view.goBack();
+                    } else {
+                        ((MainActivity)getActivity()).onBackPressed();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+        Button newPlayList = (Button) rootView.findViewById(R.id.newplaylist);
+        newPlayList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = view.getUrl();
+                String key = "";
+                int index = 0;
+                for(int i=str.length()-1; i>=0; i--) {
+                    if(str.charAt(i)=='=' && str.charAt(i-1)=='v') {
+                        index = i+1;
+                        break;
+                    }
+                }
+                if(!str.contains("&t=")){
+                    key = str.substring(index);
+                }
+                else{
+                    int end = 0;
+                    for(int j=str.length()-1;j>=0;j--) {
+                        if(str.charAt(j)=='&' && str.charAt(j+1)=='t') {
+                            end = j-1;
+                            break;
+                        }
+                    }
+                    key = str.substring(index, end);
+                }
+            }
+        });
+
+        Button showPlayList = (Button) rootView.findViewById(R.id.showplaylist);
+        showPlayList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Youtube.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView ;
+    }
+
 }
